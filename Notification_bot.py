003ec6b54938eb569c83c009dcef85602e7550d8
Ilya <Ilya.sv7490@gmail.com>
2024-12-13 11:19:00 +0500

    Коммит №3
    Добавлен пример отправки местоположения по заданным координатам

--- Notification_bot.py
+++ Notification_bot.py
@@ -7,13 +7,23 @@ TOKEN = '7799373154:AAGtLDeSLLAE11IM_P7guFUOb_GiyxGMEds'
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я бот. Здесь можно получить уведомления.")
 
+# Команда /location
+async def location(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    latitude = 55.7558  # Широта
+    longitude = 37.6173  # Долгота
+    
+    # Используем sendLocation
+    await context.bot.send_location(chat_id=update.effective_chat.id, latitude=latitude, longitude=longitude)
+    await update.message.reply_text("Вот координаты местоположения!")
+
 # Основная функция
 def main():
     # Создаем объект приложения
     application = Application.builder().token(TOKEN).build()
 
-    # Добавляем обработчик команды /start
+    # Добавляем обработчики команд
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("location", location))
 
     # Запускаем бота
     print("Бот запущен...")
